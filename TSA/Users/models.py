04df359b6c2cef0abc14452456d73c46415e7343
Tomás Ferreyra <tomaslicenciado@gmail.com>
2021-11-08 20:14:38 -0300

    ProjectSerializer, TicketSerializer, TicketDetailSerializer; corrección en Area de Usuarios

--- TSA/Users/models.py
+++ TSA/Users/models.py
@@ -4,13 +4,26 @@ from django.db.models.fields import BooleanField, EmailField
 from django.db.models.fields.related import ForeignKey
 
 
-class Area(models.Model):
-    name = models.CharField('Nombre', max_length=50)
-
-
 class User(AbstractUser):
-    area = ForeignKey(Area, on_delete=models.SET_NULL, blank=True,
-                      null=True, default=None, verbose_name='Area')
+    PROJECT_MANAGEMENT = 0
+    SALES = 1
+    REQUIREMENTS = 2
+    UX = 3
+    DEVELOPMENT = 4
+    QA = 5
+    TESTING = 6
+    MAINTENANCE = 7
+    AREAS = (
+        (PROJECT_MANAGEMENT, "Project Management"),
+        (SALES, "Sales"),
+        (REQUIREMENTS, "Requirements"),
+        (UX, "User Experience"),
+        (DEVELOPMENT, "Development"),
+        (QA, "Quality Assurance"),
+        (TESTING, "Testing"),
+        (MAINTENANCE, "Maintenance"),
+    )
+    area = models.SmallIntegerField('Area', default=PROJECT_MANAGEMENT, choices=AREAS)
     email = EmailField(unique=True)
     is_leader = BooleanField(default=False)
     USERNAME_FIELD = 'email'
