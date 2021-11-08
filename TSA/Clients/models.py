09540c499d2e1fff446d393c65c89e2809def4d5
MatiasCermak <matias.cermak99@gmail.com>
2021-11-08 19:40:29 -0300

    PostgreSQL implemented

--- TSA/Clients/models.py
+++ TSA/Clients/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 
-#Soy un puto promastercrack
+# Soy un puto promastercrack
+
 
 class Client(models.Model):
     name = models.CharField(max_length=50)
     address = models.CharField(max_length=50)
 
+
 class Contact(models.Model):
-    client = models.ForeingKey(Client, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     phone = models.CharField(max_length=15)
     mail = models.CharField(max_length=30)
-    is_main_contact: = models.BooleanField()
+    is_main_contact = models.BooleanField()
