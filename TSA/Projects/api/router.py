a90c1425b76350a5accf4f7d335d2ab3006505f8
Tomás Ferreyra <tomaslicenciado@gmail.com>
2021-11-15 12:41:08 -0300

    Limpieza

--- TSA/Projects/api/router.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import ProjectModelViewSet
-
-router_proj = DefaultRouter()
-
-router_proj.register(prefix='projects', basename='projects', viewset=ProjectModelViewSet)
