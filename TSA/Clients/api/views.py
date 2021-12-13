0bcbc5a12a438e5b306b39db1dc0c85f0e35a06c
Tomás Ferreyra <tomaslicenciado@gmail.com>
2021-12-13 20:50:52 -0300

    Permisos Clients

--- TSA/Clients/api/views.py
+++ TSA/Clients/api/views.py
@@ -1,18 +1,20 @@
-from rest_framework import status
-from rest_framework.viewsets import ViewSet, ModelViewSet
-from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 from Clients.models import Client, Contact
 from Clients.api.serializers import ClientSerializer, ContactSerializer
+from .permissions import IsSalesOnly
 
 
 class ClientModelViewSet(ModelViewSet):
     serializer_class = ClientSerializer
     queryset = Client.objects.all()
+    permission_classes = [IsSalesOnly]
 
 
 class ContactModelViewSet(ModelViewSet):
     serializer_class = ContactSerializer
     queryset = Contact.objects.all()
+    permission_classes = [IsSalesOnly]
+
     def get_queryset(self):
         queryset = self.queryset
         client = self.request.query_params.get('client')
@@ -21,52 +23,3 @@ class ContactModelViewSet(ModelViewSet):
             queryset = queryset.filter(client=client)
 
         return queryset
-
-
-# class ContactByClientViewSet(ViewSet):
-#     def retrieve(self, request, pk=None):
-#         serializer = ContactSerializer(Contact.objects.filter(client=pk), many=True)
-#         return Response(status=status.HTTP_200_OK, data=serializer.data)
-#
-# class ContactViewSet(ViewSet):
-#     def list(self, request):
-#         serializer = ContactSerializer(Contact.objects.all(), many=True)
-#         return Response(status=status.HTTP_200_OK, data=serializer.data)
-#
-#     def clientContactList(self, request, client: int):
-#         serializer = ContactSerializer(Contact.objects.all.filter(client=client), many=True)
-#         return Response(status=status.HTTP_200_OK, data=serializer.data)
-#
-#     def retrieve(self, request, pk: int):
-#         client = ContactSerializer(Contact.objects.get(pk=pk))
-#         return Response(status=status.HTTP_200_OK, data=client.data)
-#
-#     #
-#     # def list(self, request):
-#     #     serializer = ContactSerializer(Contact.objects.all(), many=True)
-#     #     return Response(status=status.HTTP_200_OK, data=serializer.data)
-#
-#     def create(self, request):
-#         serializer = ContactSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response(status=status.HTTP_200_OK, data=serializer.data)
-#
-#
-# class ClientViewSet(ViewSet):
-#     def list(self, request):
-#         serializer = ClientSerializer(Client.objects.all(), many=True)
-#         return Response(status=status.HTTP_200_OK, data=serializer.data)
-#
-#     def retrieve(self, request, pk: int):
-#         client = ClientSerializer(Client.objects.get(pk=pk))
-#         return Response(status=status.HTTP_200_OK, data=client.data)
-#
-#     def create(self, request):
-#         serializer = ClientSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response(status=status.HTTP_200_OK, data=serializer.data)
-
-
-
